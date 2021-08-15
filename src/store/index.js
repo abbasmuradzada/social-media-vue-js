0165b73c02b45984431d9c8ca718f0a0c12a814e
Elvin Huseynov <elvin.huseinov@cybernet.az>
2021-08-15 19:38:48 +0400

    bugfix

--- src/store/index.js
+++ src/store/index.js
@@ -1,96 +1,111 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import globalService from '../services/globalservice'
+import Vue from "vue";
+import Vuex from "vuex";
+import globalService from "../services/globalservice";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const userToken = localStorage.getItem("userToken")
-const userId = localStorage.getItem("userId")
-const userRole = localStorage.getItem("userRole")
+const userToken = localStorage.getItem("userToken");
+const userId = localStorage.getItem("userId");
+const userName = localStorage.getItem("userName");
+const userRole = localStorage.getItem("userRole");
 
 export default new Vuex.Store({
   state: {
     userToken,
     userId,
-    userRole
+    userName,
+    userRole,
   },
   getters: {
-    userToken: state => state.userToken,
-    userId: state => state.userId,
-    userRole: state => state.userRole,
-    isAuthenticated: state => {
-      return !!state.userToken
-    }
+    userToken: (state) => state.userToken,
+    userId: (state) => state.userId,
+    userName: (state) => state.userName,
+    userRole: (state) => state.userRole,
+    isAuthenticated: (state) => {
+      return !!state.userToken;
+    },
   },
   mutations: {
     setUserToken(state, token) {
-      state.userToken = token
-      localStorage.setItem("userToken", token)
+      state.userToken = token;
+      localStorage.setItem("userToken", token);
     },
     setUserId(state, token) {
-      state.userId = token
-      localStorage.setItem("userId", token)
+      state.userId = token;
+      localStorage.setItem("userId", token);
+    },
+    setUserName(state, token) {
+      state.userName = token;
+      localStorage.setItem("userName", token);
     },
     setUserRole(state, token) {
-      state.userRole = token
-      localStorage.setItem("userRole", token)
+      state.userRole = token;
+      localStorage.setItem("userRole", token);
     },
   },
   actions: {
     userToken({ commit }, status) {
-      commit('setUserToken', status)
+      commit("setUserToken", status);
     },
     userId({ commit }, status) {
-      commit('setUserId', status)
+      commit("setUserId", status);
+    },
+    userName({ commit }, status) {
+      commit("setUserName", status);
     },
     userRole({ commit }, status) {
-      commit('setUserRole', status)
+      commit("setUserRole", status);
     },
-    login({commit}, credentials) {
+    login({ commit }, credentials) {
       return new Promise((resolve, reject) => {
-        globalService.login(credentials)
+        globalService
+          .login(credentials)
           .then((data) => {
-            commit('setUserToken', data.token)
-            commit('setUserId', data._id)
-            commit('setUserRole', data.role)              
-            resolve(data)
+            commit("setUserToken", data.token);
+            commit("setUserId", data._id);
+            commit("setUserName", data.userName);
+            commit("setUserRole", data.role);
+            resolve(data);
           })
           .catch((err) => {
             reject(err);
-          })
-      })
+          });
+      });
     },
-    register({commit}, credentials) {
+    register({ commit }, credentials) {
       return new Promise((resolve, reject) => {
-        globalService.register(credentials)
+        globalService
+          .register(credentials)
           .then((data) => {
-            commit('setUserToken', data.token)
-            commit('setUserId', data._id)
-            commit('setUserRole', data.role)              
-            resolve(data)
+            commit("setUserToken", data.token);
+            commit("setUserId", data._id);
+            commit("setUserName", data.userName);
+            commit("setUserRole", data.role);
+            resolve(data);
           })
           .catch((err) => {
             reject(err);
-          })
-      })
+          });
+      });
     },
-    logout({commit}, router){
-      return new Promise ((resolve, reject) => {
-        globalService.logout()
+    logout({ commit }, router) {
+      return new Promise((resolve, reject) => {
+        globalService
+          .logout()
           .then((data) => {
-            commit("setUserToken", null)
-            commit("setUserId", null)
-            commit("setUserRole", null)
-            localStorage.clear()
-            router.push("/login")
-            resolve(data)
+            commit("setUserToken", null);
+            commit("setUserId", null);
+            commit("setUserName", null);
+            commit("setUserRole", null);
+            localStorage.clear();
+            router.push("/login");
+            resolve(data);
           })
           .catch((err) => {
-            reject(err)
-          })
-      })
-    }
+            reject(err);
+          });
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,120 +1,170 @@
 <template>
-	<div>
-		<div
-			v-for="post in posts"
-			:key="post"
-			class="card w-100 shadow-xss rounded-xxl border-0 p-4 mb-3"
-		>
-			<div class="card-body p-0 d-flex">
-				<router-link
-					:to="{ name: 'Profile', params: { id: post.postedUser[0]._id } }"
-				>
-					<figure class="avatar me-3">
-						<img
-							:src="post.postedUser[0].profilePicture"
-							alt="image"
-							class="shadow-sm rounded-circle w45"
-						/>
-					</figure>
-				</router-link>
-				<h4 class="fw-700 text-grey-900 font-xssss mt-1">
-					{{post.postedUser[0].userName}}
-					<span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">{{
-						post.createdAt | moment("from", "now")
-					}}</span>
-				</h4>
-			</div>
-			<div class="card-body p-0 me-lg-5">
-				<p class="fw-500 text-grey-500 lh-26 font-xssss w-100 mb-2">
-					{{ post.content }}
-				</p>
-			</div>
-			<div v-if="post.type !== 'text'" class="card-body d-block p-0 mb-3">
-				<div class="row ps-2 pe-2">
-					<div class="col-sm-12 p-1">
-						<img :src="post.postContent" class="rounded-3 w-100" alt="image" />
-					</div>
-				</div>
-			</div>
-			<div class="card-body d-flex p-0">
-				<a
-					class="emoji-bttn d-flex align-items-center fw-600 text-grey-900 text-dark lh-26 font-xssss me-2"
-				>
-					<i
-						class="feather-thumbs-up text-white bg-primary-gradiant me-1 btn-round-xs font-xss"
-					></i>
-					<!-- <i class="feather-heart text-white bg-red-gradiant me-2 btn-round-xs font-xss"></i> -->
+  <div>
+    <div
+      v-for="post in posts"
+      :key="post"
+      class="card w-100 shadow-xss rounded-xxl border-0 p-4 mb-3"
+    >
+      <div class="card-body p-0 d-flex">
+        <router-link
+          :to="{
+            name: 'Profile',
+            params: { userName: post.postedUser[0].userName },
+            query: { id: post.postedUser[0]._id },
+          }"
+        >
+          <figure class="avatar me-3">
+            <img
+              :src="post.postedUser[0].profilePicture"
+              alt="image"
+              class="shadow-sm rounded-circle w45"
+            />
+          </figure>
+        </router-link>
+        <h4 class="fw-700 text-grey-900 font-xssss mt-1">
+          {{ post.postedUser[0].userName }}
+          <span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">{{
+            formatDate(post.createdAt) | moment("from", "now")
+          }}</span>
+        </h4>
+      </div>
+      <div class="card-body p-0 me-lg-5">
+        <p class="fw-500 text-grey-500 lh-26 font-xssss w-100 mb-2">
+          {{ post.content }}
+        </p>
+      </div>
+      <div v-if="post.type === 'photo'" class="card-body d-block p-0 mb-3">
+        <div class="row ps-2 pe-2">
+          <div class="col-sm-12 p-1">
+            <img :src="post.postContent" class="rounded-3 w-100" alt="image" />
+          </div>
+        </div>
+      </div>
+      <div v-if="post.type === 'video'" class="card-body d-block p-0 mb-3">
+        <div class="row ps-2 pe-2">
+          <div class="col-sm-12 p-1">
+            <vuePlayer :src="post.postContent"> </vuePlayer>
+          </div>
+        </div>
+      </div>
+      <div class="card-body d-flex p-0">
+        <a
+          class="
+            emoji-bttn
+            d-flex
+            align-items-center
+            fw-600
+            text-grey-900 text-dark
+            lh-26
+            font-xssss
+            me-2
+          "
+        >
+          <i
+            class="
+              feather-thumbs-up
+              text-white
+              bg-primary-gradiant
+              me-1
+              btn-round-xs
+              font-xss
+            "
+          ></i>
+          <!-- <i class="feather-heart text-white bg-red-gradiant me-2 btn-round-xs font-xss"></i> -->
 
-					<v-btn @click="likeOrUnlike(post._id)" mr-2 color="red">{{
-						(post.likesFrom.indexOf(userId) != -1 ? "unlike " : "like ") +
-							post.likesFrom.length
-					}}</v-btn>
-				</a>
-				<a
-					class="d-flex align-items-center fw-600 text-grey-900 text-dark lh-26 font-xssss"
-				>
-					<i
-						class="feather-message-circle text-dark text-grey-900 btn-round-sm font-lg"
-					></i>
-					<span class="d-none-xss">{{ post.commentCount }} Comment</span>
-				</a>
-			</div>
-		</div>
-	</div>
+          <v-btn @click="likeOrUnlike(post._id)" mr-2 color="red">{{
+            (post.likesFrom.indexOf(userId) != -1 ? "unlike " : "like ") +
+            post.likesFrom.length
+          }}</v-btn>
+        </a>
+        <a
+          class="
+            d-flex
+            align-items-center
+            fw-600
+            text-grey-900 text-dark
+            lh-26
+            font-xssss
+          "
+        >
+          <i
+            class="
+              feather-message-circle
+              text-dark text-grey-900
+              btn-round-sm
+              font-lg
+            "
+          ></i>
+          <span class="d-none-xss">{{ post.commentCount }} Comment</span>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import globalservice from "../services/globalservice";
 import { mapGetters } from "vuex";
+import moment from "moment";
+import vuePlayer from "@algoz098/vue-player";
+
 export default {
-	data: () => ({
-		posts: [],
-	}),
-	computed: {
-		...mapGetters(["userId"]),
-		// test: function(){
-		//     return this.likeCount
-		// }
-	},
-	methods: {
-		getPosts() {
-			globalservice
-				.getAllPosts(this.email)
-				.then((res) => {
-					this.posts = res.data.posts;
-					this.posts.map((user) => user);
-				})
-				.catch(() => {
-					// console.log(err);
-					// console.log("test");
-				});
-		},
-		likeOrUnlike(id) {
-			globalservice
-				.likeOrUnlike(id)
-				.then(() => {
-					const index = this.posts.findIndex((post) => post._id === id);
-					if (this.posts[index].likesFrom.indexOf(this.userId) === -1) {
-						this.posts[index].likesFrom.push(this.userId);
-					} else {
-						console.log(this.posts[index].postedUser[0]._id);
-						this.posts[index].likesFrom = this.posts[index].likesFrom.filter(
-							(postedId) => postedId !== this.userId
-						);
-					}
-				})
-				.catch(() => {
-					// this.snackbarText = "Bu mailde account movcud deyil"
-					// this.snackbar = true
-				});
-		},
-		// likeMeter(liked, count){
-		//     this.likeCount = count
-		//     return liked ? 'unlike ' : 'like '
-		// }
-	},
-	created() {
-		this.getPosts();
-	},
+  data: () => ({
+    posts: [],
+  }),
+  components: {
+    vuePlayer,
+  },
+  computed: {
+    ...mapGetters(["userId"]),
+    // test: function(){
+    //     return this.likeCount
+    // }
+  },
+  methods: {
+    getPosts() {
+      globalservice
+        .getAllPosts(this.email)
+        .then((res) => {
+          this.posts = res.data.posts;
+          this.posts.map((user) => user);
+        })
+        .catch(() => {
+          // console.log(err);
+          // console.log("test");
+        });
+    },
+    likeOrUnlike(id) {
+      globalservice
+        .likeOrUnlike(id)
+        .then(() => {
+          const index = this.posts.findIndex((post) => post._id === id);
+          if (this.posts[index].likesFrom.indexOf(this.userId) === -1) {
+            this.posts[index].likesFrom.push(this.userId);
+          } else {
+            console.log(this.posts[index].postedUser[0]._id);
+            this.posts[index].likesFrom = this.posts[index].likesFrom.filter(
+              (postedId) => postedId !== this.userId
+            );
+          }
+        })
+        .catch(() => {
+          // this.snackbarText = "Bu mailde account movcud deyil"
+          // this.snackbar = true
+        });
+    },
+    // likeMeter(liked, count){
+    //     this.likeCount = count
+    //     return liked ? 'unlike ' : 'like '
+    // }
+    formatDate(postDate) {
+      const hours = moment().diff(postDate, "hours");
+      if (hours > 24) return moment(postDate).locale("az").format("LL");
+      return postDate;
+    },
+  },
+  created() {
+    this.getPosts();
+  },
 };
 </script>
