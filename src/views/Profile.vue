c262f9ca4b01bbcf051f7c4ac913ba3eddd9a382
Abbas Muradzada <abbas.muradzada@cybetnet.az>
2021-08-21 00:50:25 +0400

    like or unlike was added

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -421,12 +421,12 @@
                 font-xss
               "
 						></i>
-						<!-- <i class="feather-heart text-white bg-red-gradiant me-2 btn-round-xs font-xss"></i> -->
+						<i class="feather-heart text-white bg-red-gradiant me-2 btn-round-xs font-xss"></i>
 
-						<!-- <v-btn @click="likeOrUnlike(post._id)" mr-2 color="red">{{
+						<v-btn @click="likeOrUnlike(post._id)" mr-2 color="red">{{
 							(post.likesFrom.indexOf(userId) != -1 ? "unlike " : "like ") +
 								post.likesFrom.length
-						}}</v-btn> -->
+						}}</v-btn>
 					</a>
 					<a
 						class="
