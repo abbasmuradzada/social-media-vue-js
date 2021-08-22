6acf250e85292baeb647f192c1762cb05b8b0504
Elvin Huseynov <elvin.huseinov@cybernet.az>
2021-08-22 23:11:42 +0400

    merge

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -195,16 +195,13 @@
             </h6>
           </div>
         </div>
-        <a class="p-2 text-center ms-3 menu-icon chat-active-btn"
-          ><i class="feather-message-square font-xl text-current"></i
-        ></a>
         <a class="p-0 ms-3 menu-icon"
           ><img
             src="../assets/images/profile-4.png"
             alt="user"
             class="w40 mt--1"
         /></a>
-        <v-btn @click="logout">logout</v-btn>
+        <!-- <v-btn @click="logout">logout</v-btn> -->
       </div>
       <!-- navigation top -->
 
@@ -271,14 +268,20 @@
               </div>
               <ul class="mb-1">
                 <li class="logo d-none d-xl-block d-lg-block"></li>
-                <li class="cursor-pointer">
-                  <a
-                    @click="goToAccountSettings"
-                    class="nav-content-bttn open-font h-auto pt-2 pb-2"
-                    ><i class="font-sm feather-settings me-3 text-grey-500"></i
-                    ><span>Settings</span></a
-                  >
-                </li>
+                <router-link
+                  :to="{
+                    name: 'Settings',
+                  }"
+                >
+                  <li class="cursor-pointer">
+                    <a class="nav-content-bttn open-font h-auto pt-2 pb-2"
+                      ><i
+                        class="font-sm feather-settings me-3 text-grey-500"
+                      ></i
+                      ><span>Settings</span></a
+                    >
+                  </li>
+                </router-link>
               </ul>
             </div>
           </div>
@@ -308,7 +311,6 @@
                                     <a class="p-2 lh-20 w100 bg-primary-gradiant me-2 text-white text-center font-xssss fw-600 ls-1 rounded-xl">Confirm</a>
                                     <a class="p-2 lh-20 w100 bg-grey text-grey-800 text-center font-xssss fw-600 ls-1 rounded-xl">Delete</a>
                                 </div>
-
                                 <div class="card-body d-flex pt-0 ps-4 pe-4 pb-0">
                                     <figure class="avatar me-3"><img src="../assets/images/user-8.png" alt="image" class="shadow-sm rounded-circle w45"></figure>
                                     <h4 class="fw-700 text-grey-900 font-xssss mt-1">Mohannad Zitoun <span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">12 mutual friends</span></h4>
@@ -317,7 +319,6 @@
                                     <a class="p-2 lh-20 w100 bg-primary-gradiant me-2 text-white text-center font-xssss fw-600 ls-1 rounded-xl">Confirm</a>
                                     <a class="p-2 lh-20 w100 bg-grey text-grey-800 text-center font-xssss fw-600 ls-1 rounded-xl">Delete</a>
                                 </div>
-
                                 <div class="card-body d-flex pt-0 ps-4 pe-4 pb-0">
                                     <figure class="avatar me-3"><img src="../assets/images/user-12.png" alt="image" class="shadow-sm rounded-circle w45"></figure>
                                     <h4 class="fw-700 text-grey-900 font-xssss mt-1">Mohannad Zitoun <span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">12 mutual friends</span></h4>
@@ -327,7 +328,6 @@
                                     <a class="p-2 lh-20 w100 bg-grey text-grey-800 text-center font-xssss fw-600 ls-1 rounded-xl">Delete</a>
                                 </div>
                             </div>
-
                             <div class="card w-100 shadow-xss rounded-xxl border-0 p-0 ">
                                 <div class="card-body d-flex align-items-center p-4 mb-0">
                                     <h4 class="fw-700 mb-0 font-xssss text-grey-900">Confirm Friend</h4>
@@ -350,7 +350,6 @@
                                 </div>
                                 
                             </div>
-
                             <div class="card w-100 shadow-xss rounded-xxl border-0 mb-3 mt-3">
                                 <div class="card-body d-flex align-items-center p-4">
                                     <h4 class="fw-700 mb-0 font-xssss text-grey-900">Suggest Group</h4>
@@ -369,11 +368,8 @@
                                         <li class="ps-3 w-auto ms-1"><a class="fw-600 text-grey-500 font-xssss">Member apply</a></li>
                                     </ul>
                                 </div>
-
-
                                  
                             </div>
-
                             <div class="card w-100 shadow-xss rounded-xxl border-0 mb-3">
                                 <div class="card-body d-flex align-items-center p-4">
                                     <h4 class="fw-700 mb-0 font-xssss text-grey-900">Suggest Pages</h4>
@@ -385,18 +381,14 @@
                                 <div class="card-body d-flex align-items-center pt-0 ps-4 pe-4 pb-4">
                                     <a class="p-2 lh-28 w-100 bg-grey text-grey-800 text-center font-xssss fw-700 rounded-xl"><i class="feather-external-link font-xss me-2"></i> Like Page</a>
                                 </div>
-
                                 <div class="card-body d-flex pt-0 ps-4 pe-4 pb-0 overflow-hidden">
                                     <img src="../assets/images/g-3.jpg" alt="img" class="img-fluid rounded-xxl mb-2 bg-lightblue">
                                 </div>
                                 <div class="card-body d-flex align-items-center pt-0 ps-4 pe-4 pb-4">
                                     <a class="p-2 lh-28 w-100 bg-grey text-grey-800 text-center font-xssss fw-700 rounded-xl"><i class="feather-external-link font-xss me-2"></i> Like Page</a>
                                 </div>
-
                                  
                             </div>
-
-
                             <div class="card w-100 shadow-xss rounded-xxl border-0 mb-3">
                                 <div class="card-body d-flex align-items-center  p-4">
                                     <h4 class="fw-700 mb-0 font-xssss text-grey-900">Event</h4>
@@ -406,12 +398,10 @@
                                     <div class="bg-success me-2 p-3 rounded-xxl"><h4 class="fw-700 font-lg ls-3 lh-1 text-white mb-0"><span class="ls-1 d-block font-xsss text-white fw-600">FEB</span>22</h4></div>
                                     <h4 class="fw-700 text-grey-900 font-xssss mt-2">Meeting with clients <span class="d-block font-xsssss fw-500 mt-1 lh-4 text-grey-500">41 madison ave, floor 24 new work, NY 10010</span> </h4>
                                 </div>
-
                                 <div class="card-body d-flex pt-0 ps-4 pe-4 pb-3 overflow-hidden">
                                     <div class="bg-warning me-2 p-3 rounded-xxl"><h4 class="fw-700 font-lg ls-3 lh-1 text-white mb-0"><span class="ls-1 d-block font-xsss text-white fw-600">APR</span>30</h4></div>
                                     <h4 class="fw-700 text-grey-900 font-xssss mt-2">Developer Programe <span class="d-block font-xsssss fw-500 mt-1 lh-4 text-grey-500">41 madison ave, floor 24 new work, NY 10010</span> </h4>
                                 </div>
-
                                 <div class="card-body d-flex pt-0 ps-4 pe-4 pb-3 overflow-hidden">
                                     <div class="bg-primary me-2 p-3 rounded-xxl"><h4 class="fw-700 font-lg ls-3 lh-1 text-white mb-0"><span class="ls-1 d-block font-xsss text-white fw-600">APR</span>23</h4></div>
                                     <h4 class="fw-700 text-grey-900 font-xssss mt-2">Aniversary Event <span class="d-block font-xsssss fw-500 mt-1 lh-4 text-grey-500">41 madison ave, floor 24 new work, NY 10010</span> </h4>
@@ -1222,21 +1212,18 @@ export default {
     // goToHome(){
     //     this.$router.push('/home')
     // },
-    goToAccountSettings() {
-      this.$router.push("/home");
-    },
-    logout() {
-      return new Promise((resolve, reject) => {
-        this.$store
-          .dispatch("logout", this.$router)
-          .then((res) => {
-            resolve(res);
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      });
-    },
+    // logout() {
+    // 	return new Promise((resolve, reject) => {
+    // 		this.$store
+    // 			.dispatch("logout", this.$router)
+    // 			.then((res) => {
+    // 				resolve(res);
+    // 			})
+    // 			.catch((err) => {
+    // 				reject(err);
+    // 			});
+    // 	});
+    // },
   },
   // computed: {
   //   ...mapGetters(["userInfo"])
